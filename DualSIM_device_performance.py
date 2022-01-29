25c05cba0672cad4a260351cea7a0d470a63e799
Animesh Sachan <sachan.animesh@gmail.com>
2022-01-29 11:04:41 +0530

    first commit with python scripts

---
+++ DualSIM_device_performance.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import sqlalchemy
+import shutil
+from tqdm import tqdm
+from sqlalchemy import create_engine
+galaxy=create_engine("postgresql+psycopg2://{}:{}@redshift-cluster-2.ct9kqx1dcuaa.ap-south-1.redshift.amazonaws.com:5439/datalake".format("byogesh","W1MOi90gFZOH"))
+import datetime
+import time as tm
+import json
+import datetime as dt
+import time as tm
+#import upload_s3_to_redshift_new
+import s3_module
+import pickle
+import numpy as np
+from pymongo import MongoClient
+from math import radians, cos, sin, asin, sqrt
+import pymysql
+from datetime import datetime,timedelta
+import json
+import urllib.request
+import warnings
+import csv
+warnings.filterwarnings("ignore")
+gettime=lambda x: pd.Timestamp(x).timestamp()
+getdate=lambda x: pd.datetime.fromtimestamp(x)
+getDay=lambda x: pd.datetime.fromtimestamp(x).date()
+dttoday=int(tm.time()-tm.time()%86400-19800)
+
+pd.set_option("display.precision", 9)
+
+# take input from user
+# Day for analysys
+# yyyy-mm-dd
+analysis_day = input("Enter Day for Analysis (YYYY-MM-DD): ")
+print("Analysis Day: " + analysis_day)
+
+# read csv for vehicle numbers
+path_vNum = "/home/ubuntu/vibhor/IoT/dualsim_device_performance/vehicle_number.csv"
+vehicle_num = pd.read_csv(path_vNum)
+
+vnum_sql = ""
+for index, vnum in enumerate(vehicle_num['vehicle_number']):
+    vnum_sql += "'" + str(vnum) + "'"
+    if (index < (len(vehicle_num['vehicle_number']) - 1)):
+        vnum_sql += ','
+
+#print(vnum_sql)
+
+def haversine(L1,L2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+
+    lon1=L1[0]
+    lat1=L1[1]
+    lon2=L2[0]
+    lat2=L2[1]
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371000 # Radius of earth in meters. Use 3956 for miles
+    return c * r
+
+def ping_analysis(l):
+    vid=l[2]
+    try:
+        lc=s3_module.fetch_raw_gps(l)
+        hb=s3_module.fetch_hb(l)
+        if (len(hb)==0)&(len(lc)>0):
+            lc['created']=lc['created']/1000
+            lc.sort_values('created',inplace=True,ascending=True)
+            lc2=lc.copy()
+            lc3=lc.copy()
+
+            lc.reset_index(drop=True,inplace=True)
+            lc['date_time']=lc['time'].apply(getdate)
+            lc.set_index('date_time',inplace=True)
+            T_counts_array=lc.location.resample('T').count().values
+            min_wise=len(T_counts_array)-len(T_counts_array[T_counts_array==0])
+
+            lc2['delay']=lc2['created']-lc2['time']
+            lc2=lc2[lc2['delay']<20].reset_index(drop=True)
+            if len(lc2)>0:
+                lc2.reset_index(drop=True,inplace=True)
+                lc2['date_time']=lc2['created'].apply(getdate)
+                lc2.set_index('date_time',inplace=True)
+                T_counts_array2=lc2.location.resample('T').count().values
+                live_time=len(T_counts_array2)-len(T_counts_array2[T_counts_array2==0])
+            else:
+                live_time=0
+
+            try:
+                gsm_average=lc3['gsmPct'].mean()
+            except:
+                gsm_average=0
+            
+            heart_beat='No heartbeat'       
+
+        elif (len(hb)>0)&(len(lc)>0):
+            hb['created']=hb['created']/1000
+            hb.sort_values('created',inplace=True,ascending=True)
+            hb2=hb.copy()
+
+            hb_times=hb['created']
+            try:
+                lc_times=lc['time']
+                complete_times=hb_times.append(lc_times)
+
+            except:
+                complete_times=hb_times
+
+            random=list(range(0,len(complete_times),1))
+            combined=pd.DataFrame()
+            combined['time']=complete_times
+            combined['random']=random
+            combined['date_time']=combined['time'].apply(getdate)
+            combined.set_index('date_time',inplace=True)
+            T_counts_array=combined.time.resample('T').count().values          
+            min_wise=len(T_counts_array)-len(T_counts_array[T_counts_array==0])
+
+            lc['created']=lc['created']/1000
+            lc.sort_values('created',inplace=True,ascending=True)
+            lc['delay']=lc['created']-lc['time']
+            lc=lc[lc['delay']<20].reset_index(drop=True)
+            hb_times2=hb['created']
+            try:
+                lc_times2=lc['created']
+                complete_times2=hb_times2.append(lc_times2)
+
+            except:
+                complete_times2=hb_times2
+
+            random2=list(range(0,len(complete_times2),1))
+            combined2=pd.DataFrame()
+            combined2['time']=complete_times2
+            combined2['random']=random2
+            combined2['date_time']=combined2['time'].apply(getdate)    
+            combined2.set_index('date_time',inplace=True)
+            T_counts_array2=combined2.random.resample('T').count().values          
+            live_time=len(T_counts_array2)-len(T_counts_array2[T_counts_array2==0])
+            
+            try:
+                gsm_average=hb2['gsmPct'].mean()
+            except:
+                gsm_average=0
+            
+            heart_beat='Has heartbeat'
+
+        elif (len(hb)>0)&(len(lc)==0):
+            hb['created']=hb['created']/1000
+            hb.sort_values('created',inplace=True,ascending=True)
+            hb2=hb.copy()
+
+            hb.reset_index(drop=True,inplace=True)
+            hb['date_time']=hb['created'].apply(getdate)
+            hb.set_index('date_time',inplace=True)
+            T_counts_array=hb.voltPct.resample('T').count().values
+            min_wise=len(T_counts_array)-len(T_counts_array[T_counts_array==0])
+            live_time = min_wise
+            
+            try:
+                gsm_average=hb2['gsmPct'].mean()
+            except:
+                gsm_average=0
+
+            heart_beat='Has heartbeat' 
+
+        elif (len(hb)==0)&(len(lc)==0):
+            min_wise=0
+            live_time=0
+            gsm_average=0
+            heart_beat='No data'
+
+    except:
+        min_wise=0
+        live_time=0
+        gsm_average=0
+        heart_beat='No data' 
+        
+    return [vid,round(min_wise/14.4,2),round(live_time/14.4,2),round(gsm_average,2),heart_beat,getDay((l[0]+l[1])/2)]
+
+def run_etl():
+    print("Started at {}".format(pd.to_datetime(tm.time()+19800,unit='s')))
+    
+    try:
+        try :
+            analysis_date=dt.date()
+        except:
+            analysis_date=dt.date.today()
+
+        start=gettime(analysis_date)-106200
+        end=gettime(analysis_date)-19800
+
+        erase_date=getDay(start-(86400*2))
+        try:
+            shutil.rmtree(s3_module.get_hb_dir_path(erase_date.year, erase_date.month, erase_date.day))
+            shutil.rmtree(s3_module.get_gps_dir_path(erase_date.year, erase_date.month, erase_date.day))
+        except Exception as e:    
+            print(e)
+
+        from multiprocessing import Pool
+        pool = Pool(processes=2)
+        pool.apply_async(s3_module.downloadGpsFroms3, [start])
+        pool.apply_async(s3_module.downloadHbFroms3, [start])
+        pool.close()
+        pool.join()
+
+        query="""SELECT vehicle_id,date(actual_live_time) as installation_date,model_name
+                                    FROM analytics.vehicle_details 
+                                    WHERE date(actual_live_time) < CURRENT_DATE - INTERVAL'1 day' and 
+                                    vehicle_state='LIVE' and 
+                                    vehicle_number in ({})""".format(vnum_sql)
+        #print(query)
+        inst_veh=pd.read_sql(query , galaxy)
+
+        vid_sql = ""
+        for index, vid in enumerate(inst_veh['vehicle_id']):
+            vid_sql += str(vid)
+            if (index < (len(inst_veh['vehicle_id']) - 1)) :
+                vid_sql += ','
+        #print(vid_sql)
+
+        query="""select vehicleid as vehicle_id,date(timestamp 'epoch' + ((fromtime+19800) * interval '1 second')) as analysis_for_day,
+                        count(*) as no_info_instances,round(sum((totime-fromtime)*1.00/3600)/no_info_instances,2) as avg_no_info_hrs
+                 from trucking.trucking_mongo_devicehistory
+                 where analysis_for_day='{}' and
+                 vehicle_id in ({})  
+                 group by 1,2""".format(str(analysis_date-dt.timedelta(days=2)),vid_sql)
+        #print(query)
+        no_info_data=pd.read_sql(query , galaxy)
+        print(no_info_data)
+
+        query="""select vehicleid as vehicle_id,date(timestamp 'epoch' + ((time+19800) * interval '1 second')) as analysis_for_day,
+                        round(distance/1000,2) as total_km,round(noinfodistance/1000,2) as no_info_km
+                 from analytics.vehicledaydata
+                 where analysis_for_day='{}' """.format(str(analysis_date-dt.timedelta(days=2)))
+        distance_data=pd.read_sql(query , galaxy)
+
+        inst_veh['installation_date']=pd.to_datetime(inst_veh['installation_date'])
+
+        installed15=inst_veh
+        installed15.sort_values(by='installation_date',ascending=True,inplace=True)
+
+        analysis_of=list(installed15['vehicle_id'].unique())
+        vehicle_list=[[int(start-86400),int(end-86400),int(x)] for x in analysis_of]
+
+        final=[]
+        for j in tqdm(range(0,len(vehicle_list),1000)):
+            import multiprocessing
+            pool = multiprocessing.Pool(10)
+            results1=pool.map(ping_analysis,vehicle_list[j:(j+1000)])
+            pool.close()
+            pool.join()
+            final+=results1
+        data1=pd.DataFrame(final,columns=['vehicle_id','consistency_pct','live_pct','gsm_average','heart_beat','analysis_for_day'])
+        result3=pd.merge(data1 , installed15[['vehicle_id','model_name','installation_date']],how='left',on='vehicle_id')
+        result2=pd.merge(result3 , no_info_data, how='left',on=['vehicle_id','analysis_for_day'])
+        result=pd.merge(result2 , distance_data, how='left',on=['vehicle_id','analysis_for_day'])
+        result.fillna(0,inplace=True)
+        result['analysis_for_day']=result['analysis_for_day'].apply(lambda x : pd.to_datetime(x , format="%Y-%m-%d",errors='coerce'))
+        result['days_post_installation']=result['analysis_for_day']-result['installation_date']
+        result['days_post_installation']=result.apply(lambda x : x['days_post_installation'].days,axis=1)
+        result['online']=result['consistency_pct'].apply(lambda x : False if x==0 else True)
+        #result.to_pickle('/home/ubuntu/vibhor/IoT/master_device_performance/check.pkl')
+        print(data1)
+        print("Done1")
+        
+        #previous_data=pd.read_sql("select * from analytics.master_device_performance limit 1",galaxy)
+        #result=result[previous_data.columns]
+        #print("Done2")
+      
+        #data=result
+        #table_name='master_device_performance'
+        #update_type='append_concat'
+        #if update_type != 'update_table':
+        #    col_name=None
+        #else:
+        #    col_name=etl_data[3]
+        #print("Done3")
+        #upload_s3_to_redshift_new.upload_to_s3(data,table_name)
+        #print("Done4")
+        #upload_s3_to_redshift_new.update_execute(update_type,data,table_name,col_name)
+        #print("Done5")
+        #upload_s3_to_redshift_new.delete_from_s3(table_name)
+        #print("Done6")
+   
+    except Exception as e:
+        print("The error is: {}".format(e))
+        
+    print("Completed at {}".format(pd.to_datetime(tm.time()+19800,unit='s')))
+    
+    return None
+
+run_etl()
---
+++ S3_module.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import datetime
+import fastavro
+import os
+
+getepoch=lambda x: pd.Timestamp(x).timestamp()-19800
+getdate=lambda x: pd.datetime.fromtimestamp(x).date()
+getdatetime=lambda x: pd.datetime.fromtimestamp(x)
+
+gps_base_path = "s3://weye-archives/device/gps/year={}/month={}/date={}/"
+hb_base_path = "s3://weye-archives/device/hb/year={}/month={}/date={}/"
+
+gps_dir_path = "/data/device/gps/year={}/month={}/date={}"
+hb_dir_path = "/data/device/hb/year={}/month={}/date={}"
+
+gps_data_path = "/data/device/gps/year={}/month={}/date={}/vehicleid={}"
+hb_data_path = "/data/device/hb/year={}/month={}/date={}/vehicleid={}"
+
+def get_avro_reader(log_file):
+    if hasattr(log_file, 'read'):
+        reader = fastavro.reader(log_file)
+    else:
+        reader = None
+        try:
+            source_file = open(log_file, 'rb')
+        except Exception:
+            raise
+        else:
+            reader = fastavro.reader(source_file)
+    return reader
+
+def get_gps_base_path(year, month, date):
+    s3_path_gps = gps_base_path.format("%04d" % (year), "%02d" % (month), "%02d" % date)
+    return s3_path_gps
+
+def get_hb_base_path(year, month, date):
+    s3_path_hb = hb_base_path.format("%04d" % (year), "%02d" % (month), "%02d" % date)
+    return s3_path_hb
+
+def get_gps_dir_path(year, month, date):
+    s3_dir_gps = gps_dir_path.format("%04d" % (year), "%02d" % (month), "%02d" % date)
+    return s3_dir_gps
+
+def get_hb_dir_path(year, month, date):
+    s3_dir_hb = hb_dir_path.format("%04d" % (year), "%02d" % (month), "%02d" % date)
+    return s3_dir_hb
+
+def get_gps_data_path(year, month, date, vehicle_id):
+    s3_data_gps = gps_data_path.format("%04d" % (year), "%02d" % (month), "%02d" % date, "%d" % (vehicle_id))
+    return s3_data_gps
+
+def get_hb_data_path(year, month, date, vehicle_id):
+    s3_data_hb = hb_data_path.format("%04d" % (year), "%02d" % (month), "%02d" % date, "%d" % (vehicle_id))
+    return s3_data_hb
+   
+def downloadGpsFroms3(epochtime):
+    from_date = getdate(epochtime)
+    gps_path = get_gps_base_path(from_date.year, from_date.month, from_date.day)
+    gps_storage_path = get_gps_dir_path(from_date.year, from_date.month, from_date.day)
+    os.system("aws s3 --region ap-south-1 cp {} {} --recursive".format(gps_path,gps_storage_path))
+        
+def downloadHbFroms3(epochtime):
+    from_date = getdate(epochtime)
+    hb_path = get_hb_base_path(from_date.year, from_date.month, from_date.day)
+    hb_storage_path = get_hb_dir_path(from_date.year, from_date.month, from_date.day)
+    os.system("aws s3 --region ap-south-1 cp {} {} --recursive".format(hb_path,hb_storage_path))
+    
+def fetch_raw_gps(l2):
+    from_date = getdate(l2[0])
+    to_date = getdate(l2[1])
+    nod = int((to_date - from_date).days) #no_of_days
+    s3_gps_data_path = []
+    for i in range(0, nod + 1):
+        for_date = from_date + datetime.timedelta(days=i)
+        s3_gps_data_path.append(get_gps_data_path(for_date.year, for_date.month, for_date.day, l2[2]))
+    final_df = pd.DataFrame()  # concat data from all files to one df
+    data_len = []  # number of data_points/pings
+    count = 0
+    data_df = pd.DataFrame()  # need to remove just for testing
+    for d in s3_gps_data_path:
+        for path, subdirs, files in os.walk(d):
+            for name in files:
+                flag = False
+                final_data = []
+                count += 1
+                file_name = os.path.join(path, name)
+                try:
+                    data = get_avro_reader(file_name)
+                    flag = True
+                except Exception as e:
+                    a = 1
+                if flag:
+                    try:
+                        for record in data:
+                            final_data.append(record)
+                        data_df = pd.DataFrame(final_data)
+                        data_len.append(len(data_df))
+                        final_df = pd.concat([final_df, data_df])
+                    except Exception as e:
+                        data_len.append(e)
+                else:
+                    data_len.append("error in get_avro_reader function")
+    if len(final_df) > 0:
+        final_df.rename(columns={'createdat': 'created'}, inplace=True)
+        final_df['created'] = final_df.apply(
+            lambda x: x['created'] * 1000 if len(str(x['created'])) < 13 else x['created'], axis=1)
+        final_df = final_df.sort_values(by=['time','created'], ascending=True).reset_index(drop=True)
+        final_df = final_df.drop_duplicates(subset=['time'], keep='last').reset_index(drop=True)
+        final_df = final_df[(final_df['time'] >= l2[0]) & (final_df['time'] <= l2[1])].reset_index(drop=True)
+    gps_data = final_df
+    return gps_data
+
+def fetch_hb(l1):
+    from_date = getdate(l1[0])
+    to_date = getdate(l1[1])
+    nod = int((to_date - from_date).days) #no_of_days
+    s3_hb_data_path = []
+    for i in range(0, nod + 1):
+        for_date = from_date + datetime.timedelta(days=i)
+        s3_hb_data_path.append(get_hb_data_path(for_date.year, for_date.month, for_date.day, l1[2]))
+    final_df = pd.DataFrame()  # concat data from all files to one df
+    data_len = []  # number of data_points/pings
+    count = 0
+    data_df = pd.DataFrame()
+    for d in s3_hb_data_path:
+        for path, subdirs, files in os.walk(d):
+            for name in files:
+                flag = False
+                final_data = []
+                count += 1
+                file_name = os.path.join(path, name)
+                try:
+                    data = get_avro_reader(file_name)
+                    flag = True
+                except Exception as e:
+                    print(e)
+                if flag:
+                    try:
+                        for record in data:
+                            final_data.append(record)
+                        data_df = pd.DataFrame(final_data)
+                        data_len.append(len(data_df))
+                        final_df = pd.concat([final_df, data_df])
+                        final_df['created'] = final_df.apply(
+                            lambda x: x['created'] * 1000 if len(str(x['created'])) < 13 else x['created'], axis=1)
+                    except Exception as e:
+                        data_len.append(e)
+                else:
+                    data_len.append("error in get_avro_reader function")
+    if len(final_df) > 0:
+        final_df = final_df[(final_df['created'] >= l1[0] * 1000) & (final_df['created'] <= l1[1] * 1000)]
+    hb_data = final_df
+    return hb_data

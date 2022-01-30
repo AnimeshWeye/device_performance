f63149eab8860b8299c7a90f214bbce8a11232e4
Animesh Sachan <sachan.animesh@gmail.com>
2022-01-31 00:30:13 +0530

    edit

--- DualSIM_device_performance.py
+++ DualSIM_device_performance.py
@@ -236,8 +236,8 @@ def run_etl():
         # pool.close()
         # pool.join()
         
-        s3_module.downloadGpsFroms3(start)
-        s3_module.downloadHbFroms3(start)
+        # s3_module.downloadGpsFroms3(start)
+        # s3_module.downloadHbFroms3(start)
 
         vid_sql = ""
         for index, vid in enumerate(inst_veh['vehicle_id']):

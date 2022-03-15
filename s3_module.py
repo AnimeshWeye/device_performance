46352e13592ffab68e7d36aad1c8d791ea9fb275
Animesh Sachan <sachan.animesh@gmail.com>
2022-03-15 18:47:30 +0530

    t array print

--- s3_module.py
+++ s3_module.py
@@ -160,13 +160,13 @@ def fetch_raw_gps(l2):
                     try:
                         for record in data:
                             final_data.append(record)
-                            print(final_data)
                         data_df = pd.DataFrame(final_data)
                         data_len.append(len(data_df))
                         final_df = pd.concat([final_df, data_df])
                     except Exception as e:
                         print("error: ",e," file: ",file_name)
                         data_len.append(e)
+                        print(data_len)
                 else:
                     data_len.append("error in get_avro_reader function")
     if len(final_df) > 0:

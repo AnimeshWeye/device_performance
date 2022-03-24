e7a3f22fbb7fc00ba4b9bd91d73a9edb3f8dacc6
Animesh Sachan <sachan.animesh@gmail.com>
2022-03-25 02:07:43 +0530

    modification for new reports

--- DualSIM_device_performance_wr.py
+++ DualSIM_device_performance_wr.py
@@ -366,10 +366,10 @@ def run_etl(yr, mnth, dy):
                         vnum_undertest.append(vhnum_str)
                     # live_all_d.append(final_result['live_pct'][vindx])
                     # vnum_undertest.insert(3, 'dewhj')
-                    print(vnum_undertest)
+                    # print(vnum_undertest)
             
             # if is_vnum_added == 0:
-                # all_data.insert(0, "vehicle_number", vnum_undertest)
+            #     all_data.insert(0, "vehicle_number", vnum_undertest)
             #         vhnum_str_last = vhnum_str[len(vhnum_str) - 6 : len(vhnum_str)]
             #         search_index = final_result['vehicle_number'].str.find(vhnum_str_last)
             #         for y in range(len(final_result)) :
